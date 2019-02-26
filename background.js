25b39f6a4f4d95683a45eeecd3984249b6ca4bd7
Kunal Deshpande <kunaldeshpande11@gmail.com>
2019-02-26 22:28:34 +0530

    Fix file already downloaded
    Fixed file downloaded before Kget could capture the download

--- background.js
+++ background.js
@@ -3,21 +3,17 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    chrome.runtime.sendNativeMessage('com.kget.capture',
-        {text: "Hello"},
-        function (response) {
-
-        });
     sendResponse({farewell: "message"});
 });
 
 chrome.downloads.onCreated.addListener(function (downloadItem) {
-    chrome.runtime.sendNativeMessage('com.kget.capture',
-        {text: downloadItem.url},
-        function (response) {
-            chrome.downloads.cancel(downloadItem.id, function () {
-                chrome.downloads.erase({id: downloadItem.id}, function () {
+    chrome.downloads.cancel(downloadItem.id, function () {
+        chrome.downloads.erase({id: downloadItem.id}, function () {
+            chrome.runtime.sendNativeMessage('com.kget.capture',
+                {text: downloadItem.url},
+                function (response) {
+                    console.log(response);
                 });
-            });
         });
+    });
 });
