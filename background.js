0bf0071fe03e634fec54c0193b869b7f27fb696f
Kunal Deshpande <kunaldeshpande11@gmail.com>
2019-03-10 13:24:52 +0530

    Add option to enable or disable the extension

--- background.js
+++ background.js
@@ -1,19 +1,28 @@
 chrome.runtime.onInstalled.addListener(function () {
-
+    chrome.storage.sync.set({isEnabled: true}, function () {
+        console.log("Kget Capture Installed & Enabled Successfully");
+    });
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    sendResponse({farewell: "message"});
+    chrome.storage.sync.set({isEnabled: request.isEnabled}, function () {
+        //
+    });
+    sendResponse({message: "Kget Capture Disabled Successfully"});
 });
 
 chrome.downloads.onCreated.addListener(function (downloadItem) {
-    chrome.downloads.cancel(downloadItem.id, function () {
-        chrome.downloads.erase({id: downloadItem.id}, function () {
-            chrome.runtime.sendNativeMessage('com.kget.capture',
-                {text: downloadItem.url},
-                function (response) {
-                    console.log(response);
+    chrome.storage.sync.get('isEnabled', function (data) {
+        if (data.isEnabled) {
+            chrome.downloads.cancel(downloadItem.id, function () {
+                chrome.downloads.erase({id: downloadItem.id}, function () {
+                    chrome.runtime.sendNativeMessage('com.kget.capture',
+                        {text: downloadItem.url},
+                        function (response) {
+                            console.log(response);
+                        });
                 });
-        });
+            });
+        }
     });
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
 let isChecked = document.getElementById("isKgetEnabled");
+chrome.storage.sync.get('isEnabled', function (data) {
+    isChecked.checked = data.isEnabled;
+});
 
 isChecked.addEventListener('change', function () {
-    chrome.runtime.sendMessage({msg: 'hello'}, function (response) {
-        console.log('Message Sent ' + response.farewell);
+    chrome.storage.sync.get('isEnabled', function (data) {
+        chrome.runtime.sendMessage({isEnabled: !data.isEnabled}, function (response) {
+            console.log('Message Received: ' + response.message);
+        });
     });
 });
